13755634c4c2f43246dcf930e3af6fb47910a551
OriCruz <oc080901@gmail.com>
2022-12-20 14:08:22 -0500

    new page css done

--- views/New.jsx
+++ views/New.jsx
@@ -4,16 +4,19 @@ const DefaultLayout = require('./Default')
 class New extends React.Component{
     render(){
         return(
-            <DefaultLayout>
-                <h1>Add New Product Page</h1>
-                <form action="/products" method='POST'>
+            <DefaultLayout title={'New Product'}>
+                <link rel="stylesheet" type="text/css" href="../css/new.css"/>
+                
+                <div className='form'>
+                <form  action="/products" method='POST'>
                     Name: <input type="text" name="name"/> <br /> <br />
                     Image: <input type="text" name='img'/> <br /> <br />
                     Price: <input type="text" name='price'/> <br /> <br />
                     Description: <input type="text"  name='description'/> <br /> <br />
-                    Quantity: <input type="number"  name='quantity' min='0' max='100'/>
-                    <input type="submit" name='' value="Add Product"/>
+                    Quantity: <input type="number"  name='quantity' min='0' max='100'/> <br />
+                    <input className='add' type="submit" name='' value="Add Product"/>
                 </form>
+                </div>
             </DefaultLayout>
         );
     }
